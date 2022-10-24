f1737324fe53514a277c4b6c6e013ff19b0a56f8
gummer2004 <gummer2004@users.noreply.github.com>
2022-10-24 09:15:29 -0230

    update

--- config_sample.py
+++ config_sample.py
@@ -1,6 +1,6 @@
 mysqllogin = {
-    'user':'DATABASE_USER_NAME',
-    'password':'DATABASE_PASSWORD', 
-    'database':'DATABASE_NAME',
-    'host':'HOST_NAME_OR_IP_ADDRESS',
+    "user":"DATABASE_USER_NAME",
+    "password":"DATABASE_PASSWORD", 
+    "database":"DATABASE_NAME",
+    "host":"HOST_NAME_OR_IP_ADDRESS",
 }
--- excutable.py
+++ excutable.py
@@ -2,4 +2,4 @@ import os
 import subprocess
 
 list_files = subprocess.run("ls", "-l", capture_output=True)
-print ("the files were: %s", list_files)
+print("the files were: %s", list_files)
