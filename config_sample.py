411a8f17ac8682ef2f875ed93aa70e2886a456df
gummer2004 <gummer2004@users.noreply.github.com>
2022-10-28 09:07:09 -0230

    Update config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -1,6 +1,6 @@
 mysqllogin = {
-    "user":"DATABASE_USER_NAME",
-    "password":"DATABASE_PASSWORD", 
+    "user": "DATABASE_USER_NAME",
+    "password": "DATABASE_PASSWORD", 
     "database":"DATABASE_NAME",
-    "host":"HOST_NAME_OR_IP_ADDRESS",
+    "host": "HOST_NAME_OR_IP_ADDRESS",
 }
